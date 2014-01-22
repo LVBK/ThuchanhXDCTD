b4a05ccc29aa1656cb76d5a7687c6d0626f1d86a
LVBK <levanbaokhanh.bkhn@gmail.com>
2014-01-22 17:57:31 +0700

    Update ex1.c

--- week1/ex1.c
+++ week1/ex1.c
@@ -1,21 +1,11 @@
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
-typedef struct
-{
-  char name[81];
-  int count;
-  char line[20000];
-}data;
+#include"tree.h"
 
-typedef data elementtype;
-typedef struct node
-{
-  elementtype element;
-  struct node *left,*right;
-}node;
-typedef struct node *treetype;
 int parse(char*buff,char *part[])
 {
   char*p;
@@ -29,41 +19,6 @@ int parse(char*buff,char *part[])
   return i;
 }
 
-void printftree(treetype tree)
-{
-  if(tree!=NULL)
-    {
-      printftree(tree->left);
- printf("%s %d%s\n",tree->element.name,tree->element.count,tree->element.line);
-      printftree(tree->right);
-     
-    }
-}
-
-void freetree(treetype tree)
-{
-	if (tree!=NULL)
-	{
-		freetree(tree->left);
-		freetree(tree->right);
-		free((void*) tree);
-	}
-}
-
-void InsertNode(elementtype element,treetype *root)
-{
-if (*root == NULL){
-  *root=(node*)malloc(sizeof(node)); 
-  (*root)->element = element;
-  (*root)->left = NULL; 
-  (*root)->right = NULL; 
- }
- else if (strcmp(element.name,(*root)->element.name)==0) {(*root)->element.count++;
- strcpy(&(*root)->element.line[strlen((*root)->element.line)],element.line);
-}
- else if (strcmp(element.name,(*root)->element.name)<0) InsertNode(element, &(*root)->left);
- else if (strcmp(element.name,(*root)->element.name)>0) InsertNode(element, &(*root)->right); 
-}
 char* itostr(int num){
   char buf[10];  
   sprintf(buf,"%d",num);
@@ -74,40 +29,40 @@ char* itostr(int num){
 void standardized(char s[])
 {
 
-  	char tmp[81];
-  	int i;
+          char tmp[81];
+          int i;
 
 
-  	for(i=1;i<strlen(s)-1;i++)
-    	{
+          for(i=1;i<strlen(s)-1;i++)
+            {
 
-	      if(isupper(s[i])) s[i]=tolower(s[i]);
-	      if((s[i]<'a'||s[i]>'z')&&(s[i]<'A'||s[i]>'Z')){
-      		s[i]='\0';
-      	}
-      	}
-		  
-	
-	if((s[strlen(s)-1]<'a'||s[strlen(s)-1]>'z')&&(s[strlen(s)-1]<'A'||s[strlen(s)-1]>'Z')) {s[strlen(s)-1]='\0';}
-	if((s[0]<'a'||s[0]>'z')&&(s[0]<'A'||s[0]>'Z'))
-	      for(i=0;i<strlen(s);i++) s[i]=s[i+1];
- 	s[0]=tolower(s[0]);
+              if(isupper(s[i])) s[i]=tolower(s[i]);
+              if((s[i]<'a'||s[i]>'z')&&(s[i]<'A'||s[i]>'Z')){
+                      s[i]='\0';
+              }
+              }
+                  
+        
+        if((s[strlen(s)-1]<'a'||s[strlen(s)-1]>'z')&&(s[strlen(s)-1]<'A'||s[strlen(s)-1]>'Z')) {s[strlen(s)-1]='\0';}
+        if((s[0]<'a'||s[0]>'z')&&(s[0]<'A'||s[0]>'Z'))
+              for(i=0;i<strlen(s);i++) s[i]=s[i+1];
+         s[0]=tolower(s[0]);
 }
 treetype banword(treetype root){
-	elementtype el;
-	FILE *f;
-	char s[81];
-	if((f=fopen("stopw.txt","r"))==NULL){
-		printf("Can not open stopw.txt.\n");
-		exit(1);
-	}
-	while(!feof(f)){
-		fscanf(f,"%s\n",el.name);
-		el.count=0;
-		InsertNode(el,&root);
-	}
-	fclose(f);
-	return root;
+        elementtype el;
+        FILE *f;
+        char s[81];
+        if((f=fopen("stopw.txt","r"))==NULL){
+                printf("Can not open stopw.txt.\n");
+                exit(1);
+        }
+        while(!feof(f)){
+                fscanf(f,"%s\n",el.name);
+                el.count=0;
+                InsertNode(el,&root);
+        }
+        fclose(f);
+        return root;
 }
 
 treetype Search(char *key,treetype root)
@@ -134,38 +89,37 @@ treetype count(FILE *f,char name[],treetype root,treetype stopw)
     {printf("Can not open file %s.\n",name);
       exit(1);}
   while(fgets(s,255,f)!=NULL)
-    {	
-    	line++;
-    	int size=parse(s,part);
-    	for(i=0;i<size;i++){
-      	el.count=1;if(i>0)strcpy(temp,part[i-1]);
-      	if(i>0&&temp[strlen(temp)-1]!='.'&&isupper(part[i][0])){}
-      	else{     	
-  			strcpy(el.name,part[i]);
-  		
-			standardized(el.name);
-			strcpy(el.line,",");
- 			char*tmp=itostr(line);
- 			strcpy(&el.line[1],tmp);
-			if(strcmp(el.name,"\0")!=0&&(Search(el.name,stopw)==NULL))InsertNode(el,&root);
+    {        
+            line++;
+            int size=parse(s,part);
+            for(i=0;i<size;i++){
+              el.count=1;if(i>0)strcpy(temp,part[i-1]);
+              if(i>0&&temp[strlen(temp)-1]!='.'&&isupper(part[i][0])){}
+              else{             
+                          strcpy(el.name,part[i]);
+                  
+                        standardized(el.name);
+                        strcpy(el.line,",");
+                         char*tmp=itostr(line);
+                         strcpy(&el.line[1],tmp);
+                        if(strcmp(el.name,"\0")!=0&&(Search(el.name,stopw)==NULL))InsertNode(el,&root);
     }}}
   fclose(f);
-	return root;
+        return root;
 }
 int main()
 {
-  	FILE *f;
-  	treetype root=NULL;
-  	treetype stopw=NULL;
-  	stopw=banword(stopw);
-        char name[20];
-        printf("Enter name file: ");
-        scanf("%s",name);
-        root=count(f,name,root,stopw);
-	printftree(root);
-  	freetree(root);
-  	freetree(stopw);
-	  int choose;
-  	return 0;
+          FILE *f;
+          treetype root=NULL;
+          treetype stopw=NULL;
+          stopw=banword(stopw);
+          char name[20];
+          printf("Enter name file: ");
+          scanf("%s",name);
+          root=count(f,name,root,stopw);
+          printftree(root);
+          freetree(root);
+          freetree(stopw);
+          int choose;
+          return 0;
 }
-
